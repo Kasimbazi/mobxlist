a015b3584d2f65a3b67e85fee08be5a7fb84eee1
kasimbazi <sj623041@gmail.com>
2022-01-16 07:22:47 +0300

    Updates

---
+++ precache-manifest.643bac5b706c7722c16f8a929a9ef175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae1ad7879dc5809e89d9f8cdfaab6dc",
+    "url": "/mobxlist/index.html"
+  },
+  {
+    "revision": "74a356be98acbb5eb24b",
+    "url": "/mobxlist/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8b0fee459a16c766f207",
+    "url": "/mobxlist/static/css/main.7dcaea21.chunk.css"
+  },
+  {
+    "revision": "74a356be98acbb5eb24b",
+    "url": "/mobxlist/static/js/2.00ee10dd.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/mobxlist/static/js/2.00ee10dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0fee459a16c766f207",
+    "url": "/mobxlist/static/js/main.ce534385.chunk.js"
+  },
+  {
+    "revision": "d011a4f9b21ed8c0302a",
+    "url": "/mobxlist/static/js/runtime-main.bc51eeac.js"
+  }
+]);
